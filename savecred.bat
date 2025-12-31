176d95510fd64d796ae2a2e5f26217f6a6c40bc4
TranKuBao <116043533+TranKuBao@users.noreply.github.com>
2025-12-31 07:59:24 +0700

    Delete savecred.bat

--- savecred.bat
+++
@@ -1,7 +0,0 @@
-@if (@CodeSection == @Batch) @then
-@echo off
-start "" runas /savecred /user:admin "cmd.exe /C exit"
-CScript //nologo //E:JScript "%~F0"
-goto :EOF
-@end
-WScript.CreateObject("WScript.Shell").SendKeys("password123{ENTER}");
